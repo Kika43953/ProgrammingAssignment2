d5b516ab76edee5998148ee6a1ad03bc5d002647
Kiyotaka Kawashima <kkiyotaka0612@msn.com>
2020-09-28 20:50:37 -0700

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,17 @@
 ## first, creating fuction for cashing the invere of a Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-                m <- NULL
-                set <- function(y) {
-                  x <<- y
-                  m <<- NULL
-                }
-                get <- function() x
-                setsolve <- function(solve) m <<- solve
-                getsolve <- function() m
-                list(set = set, get = get,
-                     setsolve = setsolve,
-                     getsolve = getsolve)
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -23,13 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## second, check if cached inverse exists, if exists, retrieve the inverse from the cache. If not, computes the inverse of the matrix.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-                m <- x$getsolve()
-                if(!is.null(m)) {
-                  message("getting cached data")
-                  return(m)
-                }
-                data <- x$get()
-                m <- solve(data, ...)
-                x$setsolve(m)
-                m
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
